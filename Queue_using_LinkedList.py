4d25386a43e04953d0b78327bbcc975b9ab6a0bb
AlokMishra051298 <Mishra051298alok@gmail.com>
2020-04-08 11:38:10 +0530

    Queue using LinkedList

---
+++ Queue_using_LinkedList.py
@@ -0,0 +1,31 @@
+class Queue:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.num_elements = 0
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            self.tail.next = new_node    # add data to the next attribute of the tail (i.e. the end of the queue)
+            self.tail = self.tail.next   # shift the tail (i.e., the back of the queue)
+        self.num_elements += 1
+
+    # The dequeue method - delete the head and return value
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        data=self.head.data
+        self.head=self.head.next
+        self.num_elements-=1
+        return data
+
+    def size(self):
+        return self.num_elements
+
+    def is_empty(self):
+        return self.num_elements == 0
