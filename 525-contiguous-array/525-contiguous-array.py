6dfb7502752ce4ef4d727abfa125f291c5135719
Alok Mishra <46258899+AlokMishra051298@users.noreply.github.com>
2022-02-05 02:21:25 +0530

    Time: 942 ms (56.65%), Space: 20 MB (10.82%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.py
+++ 525-contiguous-array/525-contiguous-array.py
@@ -1,3 +1,7 @@
+'''This approach relies on the same premise as the previous approach. 
+But, we need not use an array of size 2n+1, since it isn't necessary that we'll encounter all the countcount values possible. 
+Thus, we make use of a HashMap mapmap to store the entries in the form of (index, count)(index,count). 
+We make an entry for a countcount in the mapmap whenever the countcount is encountered first, and later on use the correspoding index to find the length of the largest subarray with equal no. of zeros and ones when the same countcount is encountered again.'''
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         nums = [-1 if(x==0) else x for x in nums]
