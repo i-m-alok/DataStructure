7abc1388136bc2dcf765a22eb36bb3d6e59b3187
Alok Mishra <46258899+AlokMishra051298@users.noreply.github.com>
2022-02-07 20:04:06 +0530

    Time: 67 ms (14.25%), Space: 13.9 MB (96.95%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,6 +1,7 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         count = 0
+        # we can do it with bit manipulation also
         for i in s:
             count-=ord(i)
         for j in t:
