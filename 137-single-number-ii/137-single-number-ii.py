2dae1aabf355484704307b41066749729ee9b3b0
Alok Mishra <46258899+AlokMishra051298@users.noreply.github.com>
2022-02-16 10:33:21 +0530

    Time: 63 ms (75.47%), Space: 15.7 MB (93.23%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -2,6 +2,11 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         ones = 0
         twos = 0
+        # we know a^a=0
+        # ones will contain single occured no.
+        # two will contain no.s with occurance two
+        # one=0, two=0 means initial state or no. with occurance 3
+        # one=0, two=i means i is repeated
         for i in nums:
             ones = (ones^i)&(~twos)
             twos = (twos^i)&(~ones)
