4a3dfea54b5c59b702b7258b48ad784416faef15
Alok Mishra <46258899+AlokMishra051298@users.noreply.github.com>
2022-03-03 08:36:57 +0530

    Time: 346 ms (32.53%), Space: 15.2 MB (39.00%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,45 +1,30 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        anaMap = {}
-        result = []
-        for i in p:
-            if(anaMap.get(i)):
-                anaMap[i]+=1
-            else:
-                anaMap[i]=1
-        i=0
-        j=0
-        print(anaMap)
-        count=len(anaMap)
-        while(j<len(s)):
-            windowSize=j-i
-            # print(i,j, windowSize)
-            # print("A", anaMap)
-            if(windowSize<len(p)):
-                if(anaMap.get(s[j])!=None):
-                    anaMap[s[j]]-=1
-                    if(anaMap[s[j]]==0):
-                        count-=1
-                j+=1
-            else:
-                # print("B",i,j, count,anaMap)
-                if(count==0):
-                    result.append(i)
-                if(anaMap.get(s[i])!=None):
-                    anaMap[s[i]]+=1
-                    if(anaMap[s[i]]==1):
-                        count+=1
-                # if(anaMap.get(s[j])!=None):
-                #     anaMap[s[j]]-=1
-                #     if(anaMap[s[j]]==0):
-                #         count-=1
-                # else:
-                #     if(anaMap.get(s[i])):
-                #         anaMap[s[i]]+=1
-                i+=1
-        if(count==0):
-            result.append(i)
+        windowSize=len(p)
+        windowBuffer=[0]*26
+        startIndex=0
+        pBuffer=[0]*26
+        result=[]
+        for i in range(len(p)):
+            pBuffer[ord(p[i])-ord('a')]+=1
+        for i in range(len(s)):
+            currentWordI = ord(s[i])-ord('a')
+            windowBuffer[currentWordI]+=1
+            if((i-startIndex)>=windowSize):
+                lastWordI=ord(s[startIndex])-ord('a')
+                windowBuffer[lastWordI]-=1
+                startIndex+=1
+            if(self.validateEquality(windowBuffer, pBuffer)):
+                result.append(startIndex)
         return result
+                
+    def validateEquality(self, buffer1, buffer2):
+        for i in range(len(buffer1)):
+            if(buffer1[i]!=buffer2[i]):
+                return False
+        return True
+                
+            
             
             
             
